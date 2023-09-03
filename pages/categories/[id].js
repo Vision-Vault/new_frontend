6ade1796c056a3a8ca51f757e38c9f0aa55f5c96
Narmeen alkatab <narmeen.kattab1@gmail.com>
2023-09-03 22:48:02 +0300

    finall categoriy

--- pages/categories/[id].js
+++ pages/categories/[id].js
@@ -7,40 +7,45 @@ function CategoryPage() {
   const router = useRouter();
   const { category } = router.query;
 
- const { user, token } = useAuth();
-
+  const { user, token } = useAuth();
   const [data, setData] = useState([]);
 
-
-  async function getCtegory(id) {
+  async function getCategory(categoryId) {
     if (token) {
-      const url = baseUrl + ` /api/v1/posts/categories/${id}/`;
+      const url = `${baseUrl}/api/v1/posts/categories/${categoryId}/`;
       const option = {
         method: "GET",
         headers: {
-          Authorization: `Bearer ${token.access}`,
+         ' Authorization': `Bearer ${token.access}`,
         },
       };
-      const res = await fetch(url, option);
-      if (res.status === 200) {
-        console.log(res.status);
-        const data = await res.json();
-        setData([]);
-        data.forEach((value) => {
-          setData((json) => [...json, value]);
-        });
-      } else {
-        console.log("Failed to access protected route.");
+      try {
+        const res = await fetch(url, option);
+        if (res.status === 200) {
+          const jsonData = await res.json();
+          console.log(jsonData);
+          setData(jsonData);
+        } else {
+          console.log("Failed to access protected route.");
+        }
+      } catch (error) {
+        console.error("Error fetching category data:", error);
       }
     }
   }
 
   useEffect(() => {
-    getCtegory(category);
-  }, [token]);
+    if (category) {
+      getCategory(category); // Call the getCategory function with the category parameter
+    }
+  }, [category, token]);
+
   return (
     <div>
-
+      <h1>cccccccccccc===  </h1>
+      {data.map((item) => (
+        <div key={item.id}>{item.name}</div>
+      ))}
     </div>
   );
 }
